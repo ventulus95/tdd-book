09c7c3a60c63c3de03c42b2387675b3f50f95d27
이창섭 <ventulus95@gmail.com>
2023-10-28 01:00:46 +0900

    feat: ch3 파라미터 증가에 따른, DTO 추가

--- src/test/java/ch3/ExpiryDateCalculator.java
+++ src/test/java/ch3/ExpiryDateCalculator.java
@@ -1,9 +1,21 @@
 package ch3;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class ExpiryDateCalculator {
-	public LocalDate calculatorExpireDate(int payAmount, LocalDate billingDate) {
-		return billingDate.plusMonths(1); // 처음에는 상수 잡았다가, 차후에 형태를 틀어내버림
+	public LocalDate calculatorExpiryDate(PayData payData) { // 아래형태로 리펙토링하고...
+		LocalDate billingDate = payData.getBillingDate();
+		LocalDate firstBillingDate = payData.getFirstBillingDate();
+		int addMonth = 1;
+		if (payData.getPayAmount() >= 20000)
+			addMonth = payData.getPayAmount()/10000;
+		if (Objects.nonNull(firstBillingDate)){
+			LocalDate candidateDate = billingDate.plusMonths(addMonth);
+			if (firstBillingDate.getDayOfMonth() != billingDate.getDayOfMonth()) {
+				return candidateDate.withDayOfMonth(firstBillingDate.getDayOfMonth());
+			}
+		}
+		return billingDate.plusMonths(addMonth);
 	}
 }
--- src/test/java/ch3/ExpiryDateCalculatorTest.java
+++ src/test/java/ch3/ExpiryDateCalculatorTest.java
@@ -16,28 +16,72 @@ public class ExpiryDateCalculatorTest {
 
 	@Test
 	void 만원을_납부하면_한달_뒤가_만료일이_됨() {
-		LocalDate billingDate = LocalDate.of(2023, 10, 24);
-		LocalDate billingDate2 = LocalDate.of(2023, 9, 24);
-		int payAmount = 10000;
-
-		assertExpiryDate(payAmount, billingDate, LocalDate.of(2023, 11, 24));
-		assertExpiryDate(payAmount, billingDate2, LocalDate.of(2023, 10, 24));
-
+		assertExpiryDate(PayData.builder()
+			.payAmount(10000)
+			.billingDate(LocalDate.of(2023, 10, 24))
+			.build(), LocalDate.of(2023, 11, 24));
+		assertExpiryDate(PayData.builder()
+			.payAmount(10000)
+			.billingDate(LocalDate.of(2023, 9, 24))
+			.build(), LocalDate.of(2023, 10, 24));
 	}
 
 	//이러면 예제를 만드는 경우때문에 강재로 중복이 발생함.  중복 발생? -> 오 리팩토링할거리!
-	private void assertExpiryDate(int payAmount, LocalDate billingDate, LocalDate resultDate) {
+	private void assertExpiryDate(PayData payData, LocalDate resultDate) {
 		ExpiryDateCalculator calculator = new ExpiryDateCalculator();
-		LocalDate expiredDate = calculator.calculatorExpireDate(payAmount, billingDate);
+		LocalDate expiredDate = calculator.calculatorExpiryDate(payData);
 		assertEquals(resultDate, expiredDate);
 	}
 
 	//이제는 예외를 만들어서 처리하면됨.
 	@Test
 	void 납부일과_한달_뒤_일자가_같지_않음() {
-		assertExpiryDate(10000, LocalDate.of(2019, 1, 31), LocalDate.of(2019, 2, 28));
-		assertExpiryDate(10000, LocalDate.of(2019, 5, 31), LocalDate.of(2019, 6, 30));
+		assertExpiryDate(PayData.builder()
+			.payAmount(10000)
+			.billingDate(LocalDate.of(2019, 1, 31))
+			.build(), LocalDate.of(2019, 2, 28));
+		assertExpiryDate(PayData.builder()
+				.payAmount(10000)
+				.billingDate(LocalDate.of(2019, 5, 31))
+				.build(), LocalDate.of(2019, 6, 30));
+	}
+
+	@Test
+	void 첫_납부일과_만료일_일자가_다를때_만원_납부(){
+		PayData payData = PayData.builder()
+			.firstBillingDate(LocalDate.of(2023,1,31))
+			.billingDate(LocalDate.of(2023, 2, 28))
+			.payAmount(10000)
+			.build();
+
+		assertExpiryDate(payData, LocalDate.of(2023, 3, 31));
+
+		PayData payData2 = PayData.builder()
+			.firstBillingDate(LocalDate.of(2023,1,2))
+			.billingDate(LocalDate.of(2023, 2, 28))
+			.payAmount(10000)
+			.build();
+
+		assertExpiryDate(payData2, LocalDate.of(2023, 3, 2));
 	}
 
 
+	@Test
+	void 이만원_이상_납부시_비례해서_만료일_계산(){
+		assertExpiryDate(
+			PayData.builder()
+				.billingDate(LocalDate.of(2023, 3, 1))
+				.payAmount(20000)
+				.build(),
+			LocalDate.of(2023, 5, 1)
+		);
+		assertExpiryDate(
+			PayData.builder()
+				.billingDate(LocalDate.of(2023, 3, 1))
+				.payAmount(30000)
+				.build(),
+			LocalDate.of(2023, 6, 1)
+		);
+
+	}
 }
